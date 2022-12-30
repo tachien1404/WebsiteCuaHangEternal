15e199e12f854b0a07ac892dab43d0bd562f346d
TaQuangTuan02 <101920185+TaQuangTuan02@users.noreply.github.com>
2022-12-30 21:52:52 +0700

    timeline

--- DATN/src/main/java/webbangiaydabong/dto/OrderTimeLineDTO.java
+++ DATN/src/main/java/webbangiaydabong/dto/OrderTimeLineDTO.java
@@ -18,16 +18,19 @@ public class OrderTimeLineDTO {
     private Long id;
     private String type;
     private String description;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MMM-yyyy hh:mm:ss:aa", timezone="GMT+0700")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MMM-yyyy hh:mm:ss:aa", timezone = "GMT+0700")
     private Date create_date;
     private Long order_id;
     private Long account_id;
+    private String account_name;
 
-    public OrderTimeLineDTO(OrderTimeLine entity){
-        this.id=entity.getId();
-        this.description=entity.getDescription();
-        this.type= entity.getType();
-        this.create_date=entity.getCreate_date();
-
+    public OrderTimeLineDTO(OrderTimeLine entity) {
+        this.id = entity.getId();
+        this.description = entity.getDescription();
+        this.type = entity.getType();
+        this.create_date = entity.getCreate_date();
+        this.account_name = entity.getAccount().getFullname();
+this.account_id=entity.getAccount().getId();
+this.order_id=entity.getOrder().getId();
     }
 }
