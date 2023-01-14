deb066f13a28da35c1316c54b338d7184d9ba8de
hautvph172411 <94579770+hautvph172411@users.noreply.github.com>
2023-01-14 15:25:38 +0700

    crud+timkiem+phantrang

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/4e/b0e020d2e493001d142cf3cf71a365f8
@@ -0,0 +1,9 @@
+package webbangiaydabong.service;
+
+import webbangiaydabong.entity.Sole;
+
+import java.util.List;
+
+public interface SoleService {
+    List<Sole> findAllActice();
+}
--- DATN/src/main/java/webbangiaydabong/service/impl/SoleServiceImpl.java
+++ DATN/src/main/java/webbangiaydabong/service/impl/SoleServiceImpl.java
@@ -1,6 +1,9 @@
 package webbangiaydabong.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import webbangiaydabong.entity.Sole;
 import webbangiaydabong.repository.SoleRepository;
@@ -16,4 +19,39 @@ public class SoleServiceImpl implements SoleService {
     public List<Sole> findAllActice() {
         return soleRepository.findAllActive();
     }
+
+	@Override
+	public Sole find(long id) {
+		return soleRepository.findById(id).get();
+	}
+
+	@Override
+	public Sole create(Sole sole) {
+		return soleRepository.save(sole);
+	}
+
+	@Override
+	public Sole update(long id, Sole sole) {
+		Sole updated = soleRepository.findByIdAndIsDeleteFalse(id).get();
+		updated.setName(sole.getName());
+		return soleRepository.save(updated);
+	}
+
+	@Override
+	public void delete(long id) {
+		soleRepository.deleteById(id);
+	}
+
+	@Override
+	public Page<Sole> findAll(int page, int size) {
+		page = page <0? 0:page;
+		Pageable pageable;
+		pageable = PageRequest.of(page,size);
+		return soleRepository.getPage(pageable);
+	}
+
+	@Override
+	public List<Sole> search(String keyword) {
+		return soleRepository.findByNameLike(keyword);
+	}
 }
