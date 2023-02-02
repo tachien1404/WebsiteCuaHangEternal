09361c27df145e91dad10c899bf6413888f80dc6
tachien1404 <tachien14042002@gmail.com>
2023-02-02 10:55:32 +0700

    topSpBanChay

--- DATN/src/main/java/webbangiaydabong/repository/OrderRepository.java
+++ DATN/src/main/java/webbangiaydabong/repository/OrderRepository.java
@@ -3,15 +3,12 @@ package webbangiaydabong.repository;
 import java.util.Date;
 import java.util.List;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import webbangiaydabong.dto.OrderDTO;
-import webbangiaydabong.dto.Report;
 import webbangiaydabong.entity.Order;
 
 import javax.transaction.Transactional;
--- DATN/src/main/java/webbangiaydabong/service/impl/OrderServiceImpl.java
+++ DATN/src/main/java/webbangiaydabong/service/impl/OrderServiceImpl.java
@@ -1,31 +1,15 @@
 package webbangiaydabong.service.impl;
 
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.hibernate.transform.AliasToBeanResultTransformer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import webbangiaydabong.dto.CustomerDto;
 import webbangiaydabong.dto.OrderDTO;
 import webbangiaydabong.dto.OrderDetailDTO;
-import webbangiaydabong.dto.Report;
-import webbangiaydabong.dto.functiondto.DatHangDto;
 import webbangiaydabong.dto.functiondto.SearchDto;
 import webbangiaydabong.entity.*;
 import webbangiaydabong.repository.*;
