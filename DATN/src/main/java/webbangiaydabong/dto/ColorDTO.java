1c4180f74c30d6e5ba0b7847980b99636599c570
TaQuangTuan02 <101920185+TaQuangTuan02@users.noreply.github.com>
2023-02-06 13:12:39 +0700

    fix

--- DATN/src/main/java/webbangiaydabong/dto/ColorDTO.java
+++ DATN/src/main/java/webbangiaydabong/dto/ColorDTO.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import webbangiaydabong.entity.Color;
 
 import javax.validation.constraints.NotNull;
 
@@ -18,5 +19,9 @@ private Long id;
 @NotNull(message = "Ten mau khong duoc de trong")
 private String name;
 private String value;
-
+public ColorDTO(Color entity){
+    this.id= entity.getId();
+    this.value= entity.getValue();
+    this.name=entity.getName();
+}
 }
